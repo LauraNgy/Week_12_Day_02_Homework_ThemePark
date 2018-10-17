8ed39a9d2e1901862fd4c932dea0796a36ba0437
Laura Nagy <ngylaura@gmail.com>
2018-10-17 01:03:57 +0100

    Adds abstract Stall class

--- src/main/java/Stall.java
+++ src/main/java/Stall.java
@@ -1,2 +1,36 @@
-public abstract class Stall {
+public abstract class Stall implements IReviewed {
+
+    private String name;
+    private int rating;
+    private String ownerName;
+    private String parkingSpot;
+
+    public Stall(String name, String ownerName, String parkingSpot) {
+        this.name = name;
+        this.ownerName = ownerName;
+        this.parkingSpot = parkingSpot;
+        this.rating = 0;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    public String getOwnerName() {
+        return this.ownerName;
+    }
+
+    public String getParkingSpot() {
+        return this.parkingSpot;
+    }
+
+    @Override
+    public int getRating() {
+        return this.rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 }
